acf1c80375afb4ac980e058b4e8103b18c075490
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2022-11-21 10:20:36 +0100

    bump

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setuptools.setup(
     name='truegrad',
     license='BSD',
     description='PyTorch interface for TrueGrad-AdamW',
-    version='0.0.3',
+    version='0.0.4',
     long_description=README,
     url='https://github.com/clashluke/truegrad',
     packages=setuptools.find_packages(),
